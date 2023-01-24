752367fef41584679b69f0bb315cf50fdf07702f
sakhyat <76532498+sakhyat@users.noreply.github.com>
2023-01-24 10:27:08 -0600

    Update 02.js

--- 02.js
+++ 02.js
@@ -1,16 +1,16 @@
-function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr.length; j++) {
-        if (arr[j] > arr[j + 1]) {
-          let temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
+function binarySearch(arr,target){
+    let left=0;
+    let right=arr.length-1;
+    while(left<=right){
+        let mid=Math.floor((left+right)/2)
+        if(arr[mid]===target){
+            return mid;
+        }else if(target>arr[mid]){
+            left=mid+1;
+        }else{
+            right=mid-1;
         }
-      }
     }
-
-    return arr;
-  }
-  
-  console.log(bubbleSort([2,4,6,7,12,34,5,7,9,8]));
-
+    return -1
+}
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10],5))
